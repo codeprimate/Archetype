ddc1fd85e40d26553c56b7cb7688fa1c583d432a
Patrick Morgan <patrick.morgan@gmail.com>
2012-01-29 14:58:27 -0600

    Initial commit: Vanilla Rails 3.2.1 scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archetype::Application.config.secret_token = '5c18142e2bb4f714dd6087c3262526a774c3f0bac0f38ae6e02bc1eb042490ba797e5d241fd729614df31d6b22c6db2052329cbd6a5b4e03623140aedb42e20d'
